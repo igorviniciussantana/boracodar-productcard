69b3df6ec279665e92b7cc578ecfc494a4632a67
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-16 10:25:15 -0400

    feat: setup index head

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,12 @@ const Couch = dynamic(() => import('../components/Couch/Couch'), {ssr: false, lo
 export default function Home() {
   return (
     <>
+    <Head>
+      <link rel="shortcut icon" href="favicon.png" type="image/png" />
+      <title>#boraCodar | Product Card</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Product Card para o segundo desafio do projeto Bora Codar da Rocketseat." />
+    </Head>
     <Header />
 
     <main>
