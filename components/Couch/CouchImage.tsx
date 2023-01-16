1b47f509b99df58c3943c199722ab6d70394cd46
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-16 10:49:56 -0400

    feat(CouchImage.tsx): auto format using prettier

--- components/Couch/CouchImage.tsx
+++ components/Couch/CouchImage.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import {CouchImageWrapper} from './styled'
+import Image from "next/image";
+import { CouchImageWrapper } from "./styled";
 
-export default function CouchImage(){
-    return(
-<CouchImageWrapper>
-    <img src='/couch.png' alt="vintage couch image"/>
-</CouchImageWrapper>
-    )
+export default function CouchImage() {
+  return (
+    <CouchImageWrapper>
+      <img src="/couch.png" alt="vintage couch image" />
+    </CouchImageWrapper>
+  );
 }
