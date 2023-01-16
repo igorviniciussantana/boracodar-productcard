ce09021a3331d4955c2432a837110e0cc8dce9df
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-16 10:33:13 -0400

    style: auto format index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,30 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import dynamic from 'next/dynamic'
-import { Card, Header } from '../components/imports'
-const Couch = dynamic(() => import('../components/Couch/Couch'), {ssr: false, loading: () => <p>loading...</p>})
-
+import Head from "next/head";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+import { Card, Header } from "../components/imports";
+const Couch = dynamic(() => import("../components/Couch/Couch"), {
+  ssr: false,
+  loading: () => <p>loading...</p>,
+});
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <link rel="shortcut icon" href="favicon.png" type="image/png" />
-      <title>#boraCodar | Product Card</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="description" content="Product Card para o segundo desafio do projeto Bora Codar da Rocketseat." />
-    </Head>
-    <Header />
+      <Head>
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+        <title>#boraCodar | Product Card</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Product Card para o segundo desafio do projeto Bora Codar da Rocketseat."
+        />
+      </Head>
+      <Header />
 
-    <main>
-    <Couch />
-    <Card />
-    </main>
-   </>
-  )
+      <main>
+        <Couch />
+        <Card />
+      </main>
+    </>
+  );
 }
