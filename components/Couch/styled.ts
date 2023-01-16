60f99c216bdc172c009b482097bcb82745824a97
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-01-16 10:50:50 -0400

    feat(Couch): auto format `styled.ts` using prettier

--- components/Couch/styled.ts
+++ components/Couch/styled.ts
@@ -1,75 +1,71 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-position: relative;
-height: 100%;
-max-height: 60vh;
-min-width: 50%;
-`
+  position: relative;
+  height: 100%;
+  max-height: 60vh;
+  min-width: 50%;
+`;
 
 export const Header = styled.header`
-font-family: sans-serif;
-`
+  font-family: sans-serif;
+`;
 
 export const CouchWrapper = styled.div`
   height: 100%;
   width: 100%;
   background-color: transparent;
 
-  &:hover{
+  &:hover {
     cursor: grab;
-
   }
 
-  &:active{
+  &:active {
     cursor: grabbing;
-
   }
-`
+`;
 
 export const Button = styled.div`
-position: absolute;
-right:0;
-top: 20%;
-cursor: pointer;
-transition: .5s;
+  position: absolute;
+  right: 0;
+  top: 20%;
+  cursor: pointer;
+  transition: 0.5s;
 
-
-&:hover{
-  transform: scale(1.1);
-  transition: .5s;
-}
-`
+  &:hover {
+    transform: scale(1.1);
+    transition: 0.5s;
+  }
+`;
 
 export const CouchImageWrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
-width: 100%;
-height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 
-img{
-  width: 80%;
-}
-`
+  img {
+    width: 80%;
+  }
+`;
 
 export const Loading = styled.div`
-display: flex; 
-align-items: center;
-justify-content: center;
-position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
 
-width: 100%;
-height: 100%; 
+  width: 100%;
+  height: 100%;
 
- img{
-filter: blur(2px);
-
-}
-p{
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%)
-}
-`
+  img {
+    filter: blur(2px);
+  }
+  p {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%);
+  }
+`;
